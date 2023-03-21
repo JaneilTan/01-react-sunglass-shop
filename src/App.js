96c1131c6072fdbfb0199edda98bfce7221bf7df
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-21 20:11:53 +1300

    Completed product list component

--- src/App.js
+++ src/App.js
@@ -28,17 +28,86 @@ const SortBy = () => {
     </>
   );
 };
-const Product = () => {
+const Product = ({ image, name, description }) => {
   return (
     <li className="product-grid-item">
-          <img src={products[0].images[0]} alt={products[0].name} />
-          <h3>{products[0].name}</h3>
-          <p>{products[0].description}</p>
+          <img src={image} alt={name} />
+          <h3>{name}</h3>
+          <p>{description}</p>
           <button>Buy now</button>
         </li>
   )
 }
+const Products = () => {
+  return (
+    <>
+      <ul className="product-grid">
+        <Product 
+          image={products[0].images[0]} 
+          name={products[0].name} 
+          description={products[0].description} 
+        />
+        <Product 
+          image={products[1].images[0]} 
+          name={products[1].name} 
+          description={products[1].description} 
+        />
+        <Product 
+          image={products[2].images[0]} 
+          name={products[2].name} 
+          description={products[2].description} 
+        />
+        <Product 
+          image={products[3].images[0]} 
+          name={products[3].name} 
+          description={products[3].description} 
+        />
+        <Product 
+          image={products[4].images[0]} 
+          name={products[4].name} 
+          description={products[4].description} 
+        />
+        <Product 
+          image={products[5].images[0]} 
+          name={products[5].name} 
+          description={products[5].description} 
+        />
+        <Product 
+          image={products[6].images[0]} 
+          name={products[6].name} 
+          description={products[6].description} 
+        />
+        <Product 
+          image={products[7].images[0]} 
+          name={products[7].name} 
+          description={products[7].description} 
+        />
+        <Product 
+          image={products[8].images[0]} 
+          name={products[8].name} 
+          description={products[8].description} 
+        />
+        <Product 
+          image={products[9].images[0]} 
+          name={products[9].name} 
+          description={products[9].description} 
+        />
+        <Product 
+          image={products[10].images[0]} 
+          name={products[10].name} 
+          description={products[10].description} 
+        />
+        <Product 
+          image={products[11].images[0]} 
+          name={products[11].name} 
+          description={products[11].description} 
+        />
+        
+      </ul>
+    </>
 
+  )
+}  
 const App = () => {
   return (
     <div className="container">
@@ -47,82 +116,8 @@ const App = () => {
         <FilterBy />
         <SortBy />
       </div>
-      <ul className="product-grid">
-        <Product />
-        <li className="product-grid-item">
-          <img src={products[0].images[0]} alt={products[0].name} />
-          <h3>{products[0].name}</h3>
-          <p>{products[0].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[1].images[0]} alt={products[1].name} />
-          <h3>{products[1].name}</h3>
-          <p>{products[1].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[2].images[0]} alt={products[2].name} />
-          <h3>{products[2].name}</h3>
-          <p>{products[2].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[3].images[0]} alt={products[3].name} />
-          <h3>{products[3].name}</h3>
-          <p>{products[3].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[4].images[0]} alt={products[4].name} />
-          <h3>{products[4].name}</h3>
-          <p>{products[4].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[5].images[0]} alt={products[5].name} />
-          <h3>{products[5].name}</h3>
-          <p>{products[5].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[6].images[0]} alt={products[6].name} />
-          <h3>{products[6].name}</h3>
-          <p>{products[6].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[7].images[0]} alt={products[7].name} />
-          <h3>{products[7].name}</h3>
-          <p>{products[7].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[8].images[0]} alt={products[8].name} />
-          <h3>{products[8].name}</h3>
-          <p>{products[8].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[9].images[0]} alt={products[9].name} />
-          <h3>{products[9].name}</h3>
-          <p>{products[9].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[10].images[0]} alt={products[10].name} />
-          <h3>{products[10].name}</h3>
-          <p>{products[10].description}</p>
-          <button>Buy now</button>
-        </li>
-        <li className="product-grid-item">
-          <img src={products[11].images[0]} alt={products[11].name} />
-          <h3>{products[11].name}</h3>
-          <p>{products[11].description}</p>
-          <button>Buy now</button>
-        </li>
-      </ul>
-    </div>
+        <Products />
+      </div>
   );
 };
 
